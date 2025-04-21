626b1781d4be6b35aad360b53805c695af273471
muratsa <murat.sayrim@kafein.com.tr>
2025-04-21 16:04:22 +0300

    initial3

--- src/main/java/com/kafein/portalauth/config/KeyCloakConfiguration.java
+++ src/main/java/com/kafein/portalauth/config/KeyCloakConfiguration.java
@@ -1,14 +1,15 @@
 package com.kafein.portalauth.config;
 
 import lombok.Data;
-import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
 @Data
 @Configuration
-@ConfigurationProperties(prefix = "jwt.auth.converter")
 public class KeyCloakConfiguration {
+    @Value("${jwt.auth.converter.resource-id}")
     private String resourceId;
+    @Value("${jwt.auth.converter.principle-attribute}")
     private String principleAttribute;
 }
 
