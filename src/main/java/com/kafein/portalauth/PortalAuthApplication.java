0d3dca039f2526163841cb73017d61a2bbe23dfc
muratsa <murat.sayrim@kafein.com.tr>
2025-04-21 16:40:01 +0300

    initial4

--- src/main/java/com/kafein/portalauth/PortalAuthApplication.java
+++ src/main/java/com/kafein/portalauth/PortalAuthApplication.java
@@ -3,7 +3,7 @@ package com.kafein.portalauth;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.kafein.portalauth")
 public class PortalAuthApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/kafein/portalauth/config/KeyCloakConfiguration.java
+++ src/main/java/com/kafein/portalauth/config/KeyCloakConfiguration.java
@@ -4,12 +4,30 @@ import lombok.Data;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
-@Data
+
 @Configuration
 public class KeyCloakConfiguration {
     @Value("${jwt.auth.converter.resource-id}")
     private String resourceId;
     @Value("${jwt.auth.converter.principle-attribute}")
     private String principleAttribute;
+
+    // Getter ve Setter metodları
+
+    public String getResourceId() {
+        return resourceId;
+    }
+
+    public void setResourceId(String resourceId) {
+        this.resourceId = resourceId;
+    }
+
+    public String getPrincipleAttribute() {
+        return principleAttribute;
+    }
+
+    public void setPrincipleAttribute(String principleAttribute) {
+        this.principleAttribute = principleAttribute;
+    }
 }
 
